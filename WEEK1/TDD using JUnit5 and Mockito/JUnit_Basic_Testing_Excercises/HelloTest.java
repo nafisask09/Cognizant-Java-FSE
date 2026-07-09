c960e04ecebbc193c50d275ed80f1f8f03fa0b51
nafisask09 <nafisask2005@gmail.com>
2026-07-09 13:21:13 +0530

    Add files via upload

---
+++ WEEK1/TDD using JUnit5 and Mockito/JUnit_Basic_Testing_Excercises/HelloTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
